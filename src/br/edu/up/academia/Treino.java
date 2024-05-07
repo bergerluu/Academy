89b6b1c1ba80d77202920920f78f5b298397ea10
Luiz Felipe <126410649+bergerluu@users.noreply.github.com>
2024-05-07 19:34:25 -0300

    Update Treino.java

--- src/br/edu/up/academia/Treino.java
+++ src/br/edu/up/academia/Treino.java
@@ -19,20 +19,21 @@ public class Treino {
         this.aluno = aluno;
         this.equipamentos = new ArrayList<>();
     }
-
+    // Printa para o usuario o "Nome do treino, do Instrutor e do Aluno"
     public static void listarTreinos(Treino treino) {
         System.out.println("Nome do treino: " + treino.getNome());
         System.out.println("Instrutor: " + treino.getInstrutor().getNome());
         System.out.println("Nome do aluno: " + treino.getAluno().getNome());
-        for(Equipamento equipamentos : treino.getEquipamento()){
-            System.out.println("- " + treino.getEquipamento());
-        }
     }
     public static void listarEquipamentosOrdenados(Treino treino) {
+        // Cria uma nova lista de equipamentos chamada "equipamentos", inicializando-a com os equipamentos do treino
         List<Equipamento> equipamentos = new ArrayList<>(treino.getEquipamento());
+        // Esta linha ordena os equipamentos na lista equipamentos com base na ordem alfabética
+        //  Usa o método "sort", que passa um comparador para comparar os nomes dos equipamentos
         equipamentos.sort(Comparator.comparing(Equipamento::getNomeEquipamento));
-
+        //  Percorre sobre cada elemento
         for (Equipamento equipamento : equipamentos) {
+            // Printa em ordem cada equipamento
             System.out.println("- " + " (" + equipamento.getNomeEquipamento() + ")");
         }
     }
