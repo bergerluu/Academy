671557f994a73bdd01dc9f83e8c9c1e8f698e0c7
Luiz Felipe <126410649+bergerluu@users.noreply.github.com>
2024-05-07 19:31:19 -0300

    Update Equipamento.java

--- src/br/edu/up/academia/Equipamento.java
+++ src/br/edu/up/academia/Equipamento.java
@@ -14,12 +14,16 @@ public class Equipamento {
     public String getNomeEquipamento() {
         return nomeEquipamentos;
     }
+    // Obejto do tipo "Equipamento" que recebe 2 parametros, "treino" e "nomeEquipamento"    
     public static Equipamento pesquisarEquipamento(Treino treino, String nomeEquipamento) {
         for (Equipamento equipamento: treino.getEquipamento()){
+             //Verificação para ver se o nome do Equipamento é igual ao nome do Equipamento passado como parâmetro
             if (equipamento.getNomeEquipamento().equals(nomeEquipamento)){
+                // Se o equipamento com nome específico for encontrado, ele é retornado
                 return equipamento;
             }
         }
+        //Se nao houver equipamento com o nome específico, retorna null
         return null;
 
     }
